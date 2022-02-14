5d0ab60286f69af354e92718f3c2a9126a52e9f6
nblkmal <nblkmal@gmail.com>
2022-02-14 17:24:18 +0800

    employee-create : add form and method

--- resources/js/components/employees/Create.vue
+++ resources/js/components/employees/Create.vue
@@ -1,27 +1,27 @@
 <template>
     <div class="text-2xl text-gray-700">
         Employee Create
-        <form>
+        <form @submit.prevent="storeEmployee">
             <div class="mb-6">
                 <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First Name</label>
-                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
+                <input v-model="form.first_name" type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Thomas" required>
             </div>
             <div class="mb-6">
-                <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First Name</label>
-                <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
+                <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Last Name</label>
+                <input v-model="form.last_name" type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Shelby" required>
             </div>
             <div class="mb-6">
-                <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First Name</label>
-                <input type="text" id="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
+                <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Address</label>
+                <input v-model="form.address" type="text" id="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Small Heath, Birmingham" required>
             </div>
-            <div class="flex items-start mb-6">
+            <!-- <div class="flex items-start mb-6">
                 <div class="flex items-center h-5">
                 <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
                 </div>
                 <div class="ml-3 text-sm">
                 <label for="remember" class="font-medium text-gray-900 dark:text-gray-300">Remember me</label>
                 </div>
-            </div>
+            </div> -->
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </form>
     </div>
@@ -29,6 +29,29 @@
 
 <script>
 export default {
-    
+    data() {
+        return {
+            form: {
+                first_name: '',
+                last_name: '',
+                address: '',
+            }
+        };
+    },
+    // untuk get data terus macam dropdown data
+    // created() {
+
+    // },
+    methods: {
+        storeEmployee() {
+            axios.post("/api/employee/store", {
+                'first_name': this.form.first_name,
+                'last_name': this.form.last_name,
+                'address': this.form.address,
+            }).then(res => {
+                console.log(res)
+            })
+        }
+    }
 }
 </script>
