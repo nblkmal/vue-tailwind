b4700bac74b1edab260652216485dc9f5d223ed2
nblkmal <nblkmal@gmail.com>
2022-02-14 18:10:10 +0800

    update employee index vue

--- resources/js/components/employees/Index.vue
+++ resources/js/components/employees/Index.vue
@@ -2,13 +2,33 @@
     <div class="text-2xl text-gray-700">
         Employee Index
         <router-link to="/employees-create" class="bg-cyan-900 text-white rounded-md px-7 py-3 uppercase transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-cyan-500 duration-300 ">+ employee</router-link>
-        <!-- <button class="bg-cyan-900 text-white rounded-md px-7 py-3 uppercase transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-cyan-500 duration-300 ">+ employee</button> -->
-        
+        <div class="my-5" v-for="(employee, key) in employees" :key="employee.id">
+            <div class="text-sm">{{ key }} - {{ employee.first_name }}, {{ employee.last_name }}</div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            employees: [],
+        }
+    },
+    created() {
+        this.getEmployees();
+    },
+    methods: {
+        getEmployees() {
+            axios.get("/api/employee/index")
+            .then(res => {
+                this.employees = res.data.data;
+                console.log(res);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+    }
 }
 </script>
