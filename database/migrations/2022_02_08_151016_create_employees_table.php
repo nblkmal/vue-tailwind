d2b05d355d580f5d4086aae68e3f8c9ba93dbbed
nblkmal <nblkmal@gmail.com>
2022-02-14 17:23:34 +0800

    add nullable()

--- database/migrations/2022_02_08_151016_create_employees_table.php
+++ database/migrations/2022_02_08_151016_create_employees_table.php
@@ -18,10 +18,10 @@ class CreateEmployeesTable extends Migration
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('address')->nullable();
-            $table->foreignId('department_id')->constrained();
-            $table->foreignId('country_id')->constrained();
-            $table->foreignId('state_id')->constrained();
-            $table->foreignId('city_id')->constrained();
+            $table->foreignId('department_id')->nullable()->constrained();
+            $table->foreignId('country_id')->nullable()->constrained();
+            $table->foreignId('state_id')->nullable()->constrained();
+            $table->foreignId('city_id')->nullable()->constrained();
             $table->char('zip_code')->nullable();
             $table->date('birthdate')->nullable();
             $table->date('date_hired')->nullable();
