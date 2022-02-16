e94c7f6d1bb9499df983bf6bcbe660f777468c3f
nblkmal <nblkmal@gmail.com>
2022-02-16 16:50:29 +0800

    add api employee edit function

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -26,11 +26,36 @@ class EmployeeController extends Controller
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
             'address' => $request->address,
+            'country_id' => $request->country_id,
         ]);
 
         return response()->json([
             'status' => true,
             'message' => 'Successfully store employee',
+            'data' => $employee,
+        ]);
+    }
+
+    public function edit(EmployeeRequest $request, Employee $employee)
+    {
+        $employee->update([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'address' => $request->address,
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Successfully update employee '.$employee->first_name,
+            'data' => $employee,
+        ]);
+    }
+
+    public function show(Employee $employee)
+    {
+        return response()->json([
+            'status' => true,
+            'message' => 'Successfully fetch data employee '.$employee->first_name,
             'data' => $employee,
         ]);
     }
