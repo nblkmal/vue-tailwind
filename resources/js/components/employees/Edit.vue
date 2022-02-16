bbf1ecea51650ee939249cdbe9cb2d30a6c2ca2e
nblkmal <nblkmal@gmail.com>
2022-02-16 16:51:53 +0800

    update edit employee function

--- resources/js/components/employees/Edit.vue
+++ resources/js/components/employees/Edit.vue
@@ -1,11 +1,60 @@
 <template>
     <div class="text-2xl text-gray-700">
-        Employee Edit
+        Employee Edit 
+        <form @submit.prevent="editEmployee">
+            <div class="mb-6">
+                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First Name</label>
+                <input v-model="form.first_name" type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+            </div>
+            <div class="mb-6">
+                <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Last Name</label>
+                <input v-model="form.last_name" type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+            </div>
+            <div class="mb-6">
+                <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Address</label>
+                <input v-model="form.address" type="text" id="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Small Heath, Birmingham" required>
+            </div>
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        </form>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            form: {
+                first_name: '',
+                last_name: '',
+                address: '',
+            }
+        };
+    },
+    // untuk get data terus macam dropdown data
+    created() {
+        this.getEmployee();
+    },
+    methods: {
+        getEmployee() {
+            axios.get("/api/employee/show/"+ this.$route.params.id)
+            .then(res => {
+                console.log(res.data.data)
+                this.form = res.data.data;
+            })
+            .catch(error => {
+                    console.log(console.error);
+                });
+        },
+        editEmployee() {
+            axios.post("/api/employee/edit/"+ this.$route.params.id, {
+                'first_name': this.form.first_name,
+                'last_name': this.form.last_name,
+                'address': this.form.address,
+            })
+            .then(res => {
+                this.$router.push({name: 'EmployeeIndex'});
+            });
+        }
+    }
 }
 </script>
