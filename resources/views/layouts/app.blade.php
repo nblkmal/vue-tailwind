568864882b7d6ec1a9ccc9d9312676e69e27373e
nblkmal <nblkmal@gmail.com>
2022-02-17 14:17:40 +0800

    remove button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,9 +34,6 @@
                     <li class="cursor-pointer">
                         <router-link class="text-black bg-gray-200 py-2 px-4 rounded-md text-center inline-flex items-center" to="/about">About</router-link>
                     </li>
-                    <li class="cursor-pointer">
-                        <button>Button</button>
-                    </li>
                     <li class="cursor-pointer ">
                         <button id="dropdown-employee-button" data-dropdown-toggle="dropdown-employee" class="text-black bg-gray-200 py-2 px-4 rounded-md text-center inline-flex items-center" type="button">Employee <svg class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
                         <!-- Dropdown menu -->
