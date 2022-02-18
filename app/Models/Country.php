3104f46799676a74bfeba2cd94db2a480558fae0
nblkmal <nblkmal@gmail.com>
2022-02-18 17:42:03 +0800

    add employees relationship

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\State;
+use App\Models\Employee;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -18,5 +19,10 @@ class Country extends Model
     public function states()
     {
         return $this->hasMany(State::class);
+    }
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
     }
 }
