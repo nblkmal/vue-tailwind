76a513b3568b68bc7b44f77ffb6a023756f26030
nblkmal <nblkmal@gmail.com>
2022-02-18 17:42:39 +0800

    change state id seeder

--- database/seeders/StateSeeder.php
+++ database/seeders/StateSeeder.php
@@ -17,32 +17,32 @@ class StateSeeder extends Seeder
         DB::table('states')->insert([
             [
                 'id' => 1,
-                'country_id' => 1,
+                'country_id' => 163,
                 'name' => 'Selangor'
             ],
             [
                 'id' => 2,
-                'country_id' => 1,
+                'country_id' => 163,
                 'name' => 'Perak'
             ],
             [
                 'id' => 3,
-                'country_id' => 1,
+                'country_id' => 163,
                 'name' => 'Negeri Sembilan'
             ],
             [
                 'id' => 4,
-                'country_id' => 1,
+                'country_id' => 163,
                 'name' => 'Pahang'
             ],
             [
                 'id' => 5,
-                'country_id' => 3,
+                'country_id' => 203,
                 'name' => 'Changi'
             ],
             [
                 'id' => 6,
-                'country_id' => 3,
+                'country_id' => 203,
                 'name' => 'Ang Mo Kio'
             ],
         ]);
