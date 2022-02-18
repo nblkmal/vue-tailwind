80250d965145d9bb4d090f2bc7d8f105c9568a76
nblkmal <nblkmal@gmail.com>
2022-02-18 17:43:48 +0800

    add datepicker cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="https://unpkg.com/flowbite@1.3.3/dist/flowbite.js"></script>
+    <script src="https://unpkg.com/flowbite@1.3.4/dist/datepicker.js"></script>
     {{-- <script src="../path/to/flowbite/dist/flowbite.js"></script> --}}
 </head>
 <body class="font-Poppins">
