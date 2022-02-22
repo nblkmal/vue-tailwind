a44f77ea9a0e685dd9cde7258b9e958762fb2398
nblkmal <nblkmal@gmail.com>
2022-02-22 15:28:39 +0800

    update department index

--- resources/js/components/departments/Index.vue
+++ resources/js/components/departments/Index.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="text-2xl text-gray-700">
         Department Index
-        <!-- <router-link to="/employees-create" class="float-right text-xs bg-cyan-900 text-white rounded-md px-7 py-3 uppercase transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-cyan-500 duration-300 ">+ employee</router-link> -->
         <div class="mt-5 grid grid-cols-4 gap-4">
             <div v-for="department in departments" :key="department.id" class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
-                    <img class="rounded-t-lg" src="https://www.cyberjayacity.com/wp-content/uploads/2018/07/tamarind-square-cyberjaya-1280x720.jpg" alt="" />
+                    <img v-if="department.image" class="rounded-t-lg" :src="'../storage/department/'+ department.image" alt="" />
+                    <img v-else class="rounded-t-lg" src="https://happygokl.com/wp-content/uploads/Tamarind-square-4.jpg" alt="" />
                 </a>
                 <div class="p-5">
                     <a href="#">
@@ -13,27 +13,28 @@
                             {{ department.name }}
                         </h5>
                     </a>
-                    <p class="mb-3 text-sm text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <p class="mb-3 text-sm text-gray-700 dark:text-gray-400">
+                        {{ department.description }}
+                    </p>
+                    <button @click.prevent="toggleEditModal(department)" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Edit<i class="ml-2 fas fa-pen"></i>
-                    </a>
+                    </button>
 
                     <button @click.prevent="toggleDeleteModal(department)" class="bg-red-700 text-white p-2 rounded text-sm">Delete</button>
 
-                    
-                    
                 </div>
             </div>
             <div class="flex items-center bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <div class="py-5 m-auto text-center">
-                    <i class="fas fa-plus"></i>
+                <div class="py-5 m-auto text-center" type="button" @click.prevent="toggleAddModal()">
+                    <i class="fas fa-plus hover:bg-gray-200 hover:rounded-full"></i>
                     <div class="text-xs">Add new department</div>
                 </div>
             </div>
         </div>
+        <EditModal :department.sync="toEdit" v-if="isEditVisibility" @toggle-modal="toggleEditModal()"/>
+        <AddModal v-if="isAddVisibility" @toggle-modal="toggleAddModal()"/>
         <DeleteModal :department.sync="toDelete" v-if="isVisibility" @toggle-modal="toggleDeleteModal(toDelete)"/>
-        
-        
+
         
     </div>
 </template>
@@ -41,22 +42,23 @@
 <script>
 import DeleteModal from "../modal/department/DeleteModal"
 import AddModal from "../modal/department/AddModal"
+import EditModal from "../modal/department/EditModal"
 
 export default {
-    props: {
-        title: {
-            type: String,
-        }
-    },
     components: {
         DeleteModal,
+        AddModal,
+        EditModal,
     },
     data()
     {
         return {
             departments: [],
             isVisibility: false,
+            isAddVisibility: false,
+            isEditVisibility: false,
             toDelete: null,
+            toEdit: null,
         }
     },
     created()
@@ -64,10 +66,23 @@ export default {
         this.getDepartments();
     },
     methods: {
+        toggleEditModal(department)
+        {
+            console.log(department)
+            this.toEdit = department
+            this.isEditVisibility = !this.isEditVisibility
+            this.getDepartments()
+        },
+        toggleAddModal()
+        {
+            this.isAddVisibility = !this.isAddVisibility
+            this.getDepartments()
+        },
         toggleDeleteModal(department)
         {
             this.toDelete = department
             this.isVisibility = !this.isVisibility
+            this.getDepartments();
         },
         getDepartments()
         {
