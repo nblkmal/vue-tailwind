3f0f42a1b2a24f75281b8715d73b165d7dd37d7c
nblkmal <nblkmal@gmail.com>
2022-02-22 15:28:55 +0800

    update delete modal

--- resources/js/components/modal/department/DeleteModal.vue
+++ resources/js/components/modal/department/DeleteModal.vue
@@ -41,6 +41,7 @@ export default {
             .then((res) => {
                 console.log(res)
                 this.closeModal()
+                // this.getDepartments()
             });
         }
     }
